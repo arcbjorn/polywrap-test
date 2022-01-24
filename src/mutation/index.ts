7c6bbcc27c6af16856dc6690013df13ade8afbbf
Oleg Luganskiy <arky.happ@gmail.com>
2022-01-24 21:29:51 +0300

    feat (mutation): add setIpfsData fn

--- src/mutation/index.ts
+++ src/mutation/index.ts
@@ -1,7 +1,10 @@
 import {
   Ethereum_Mutation,
   Input_setData,
-  Input_deployContract
+  Input_deployContract,
+  Ipfs_Mutation,
+  Input_setIpfsData,
+  SetIpfsDataResult,
 } from "./w3";
 import { abi, bytecode } from "../contracts/SimpleStorage";
 
@@ -10,7 +13,7 @@ export function setData(input: Input_setData): string {
     address: input.address,
     method: "function set(uint256 value)",
     args: [input.value.toString()],
-    connection: input.connection
+    connection: input.connection,
   });
 
   return res.hash;
@@ -21,6 +24,27 @@ export function deployContract(input: Input_deployContract): string {
     abi,
     bytecode,
     args: null,
-    connection: input.connection
+    connection: input.connection,
   });
+}
+
+export function setIpfsData(input: Input_setIpfsData): SetIpfsDataResult {
+  // 1. Upload the data to IPFS
+  const ipfsHash = Ipfs_Mutation.addFile({
+    data: String.UTF8.encode(input.options.data),
+  });
+
+  // 2. Add the data's IPFS hash to SimpleStorage using `setHash(...)`
+  const txReceipt = Ethereum_Mutation.callContractMethodAndWait({
+    address: input.options.address,
+    method: "function setHash(string value)",
+    args: [ipfsHash],
+    connection: input.connection,
+  });
+
+  // 3. Return the result
+  return {
+    ipfsHash,
+    txReceipt: txReceipt.transactionHash,
+  };
 }
